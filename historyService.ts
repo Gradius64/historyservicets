6d5f370c60d5b0e78f2d4c12be984bbdb967eb62
Draca Baugh-Flowers <66185683+Gradius64@users.noreply.github.com>
2024-10-27 15:18:32 -0500

    Add files via upload

---
+++ historyService.ts
@@ -0,0 +1,87 @@
+// TODO: Define a City class with name and id properties
+class City {
+  name: string;
+  id: number;
+
+  constructor(name: string, id: number) {
+    this.name = name;
+    this.id = id;
+  }
+}
+// TODO: Complete the HistoryService class
+class HistoryService {
+  // TODO: Define a read method that reads from the searchHistory.json file
+ 
+            
+         
+
+  // Function to read from the searchHistory.json file
+
+
+
+
+
+  
+  
+  
+  
+  
+  
+    
+ 
+
+  
+      
+    
+     
+  
+ 
+   // Output: 10
+  // myObj.readValue = 20; // Error: Cannot assign to 'readValue' because it is a read-only property.
+    
+        
+
+    
+       
+            
+
+
+  
+  
+   
+      
+  
+   
+  // private async read() {}
+  // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
+  
+
+  
+   
+
+
+
+  
+  
+
+  // private async write(cities: City[]) {}
+  // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
+  
+
+
+  // async getCities() {}
+  // TODO Define an addCity method that adds a city to the searchHistory.json file
+  
+  
+ 
+  // async addCity(city: string) {}
+  // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
+  // async removeCity(id: string) {}
+
+
+
+
+
+
+}
+  
